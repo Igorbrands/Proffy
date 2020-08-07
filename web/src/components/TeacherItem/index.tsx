31c3f636f8f41e9eb225372ca6a799c7310a6022
igorbrands <igorcantelmo@gmail.com>
2020-08-07 03:13:35 -0300

    api linking front and back ends

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,47 +1,60 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
-import daiImg from '../../assets/images/dai.jpg';
+
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src={daiImg} alt="Daiane Crivelaro" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Daiane Crivelaro</strong>
-          <span>Português/Literatura/Redação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Daiane Crivelaro é doutoranda em literatura pela UFF; mestra e graduada
-        pela UFRJ.
-        <br />
-        <br />
-        Hoje, atua como professora efetiva do Colégio Pedro II. Em anos
-        anteriores, foi professora do CEFET-RJ, Eliezer Max, Colégio de
-        Aplicação da Universidade do Estado do Rio de Janeiro, do Colégio de
-        Aplicação da Universidade Federal do Rio de Janeiro e do Sistema pH de
-        Ensino. Sob orientação de Stefania Chiarelli, pesquisa, atualmente, a
-        escrita literária feminina e busca, com isso, trazer os estudos para o
-        campo prático da educação básica.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$150,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          onClick={createNewConnection}
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="WhatsApp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
